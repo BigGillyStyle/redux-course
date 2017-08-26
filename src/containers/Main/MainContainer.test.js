92d5554b3f138fc8c605864fb4491bf4eadae09e
Andy Pickler <andy.pickler@gmail.com>
2017-08-26 06:41:23 -0600

    Add MainContainer

--- src/containers/Main/MainContainer.test.js
+++ src/containers/Main/MainContainer.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import MainContainer from "./MainContainer";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<MainContainer />, div);
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import MainContainer from "./containers/Main/MainContainer";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MainContainer />, document.getElementById("root"));
 registerServiceWorker();
